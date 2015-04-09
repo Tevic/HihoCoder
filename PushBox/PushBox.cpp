0b28fa887d86d606b01765c5bc395db8973280c4
Tevic <tevic.tt@gmail.com>
2015-04-09 13:22:05 +0800

    Fix Push Box

--- PushBox/PushBox.cpp
+++ PushBox/PushBox.cpp
@@ -1,106 +1,93 @@
 #include "../Head.h"
 
 
-void Up(vector<vector<int> > &board, int &i, int &j, int &bX, int &bY)
+void Move(vector<vector<int> > &board, char chDirection, int &pX, int &pY, int &bX, int &bY)
 {
 	int N = board[0].size();
 	int M = board.size();
-	if (i - 1 >= 0 && board[i - 1][j] != 4)
+	switch (chDirection)
 	{
-		if (board[i - 1][j] == 3)
+	case 'l':
+	{
+		if (pY - 1 >= 0 && board[pX][pY - 1] != 4)
 		{
-			if (i - 2 >= 0)
+			if (pY - 1 == bY&&pX == bX)
 			{
-				board[i - 2][j] = 3;
-				board[i - 1][j] = 1;
-				board[i][j] = 0;
-				i--;
-				bX--;
+				if (bY - 1 >= 0 && board[bX][bY - 1] != 4)
+				{
+					bY--;
+					pY--;
+				}
+			}
+			else
+			{
+				pY--;
 			}
 		}
-		else
-		{
-			board[i - 1][j] = 1;
-			board[i][j] = 0;
-			i--;
-		}
+		break;
 	}
-}
-void Down(vector<vector<int> > &board, int &i, int &j, int &bX, int &bY)
-{
-	int N = board[0].size();
-	int M = board.size();
-	if (i + 1 < M && board[i + 1][j] != 4)
+	case 'r':
 	{
-		if (board[i + 1][j] == 3)
+		if (pY + 1 < N && board[pX][pY + 1] != 4)
 		{
-			if (i + 2 >= 0)
+			if (pY + 1 == bY&&pX == bX)
 			{
-				board[i + 2][j] = 3;
-				board[i + 1][j] = 1;
-				board[i][j] = 0;
-				i++;
-				bX++;
+				if (bY + 1 < N&&board[bX][bY + 1] != 4)
+				{
+					bY++;
+					pY++;
+				}
+			}
+			else
+			{
+				pY++;
 			}
 		}
-		else
-		{
-			board[i + 1][j] = 1;
-			board[i][j] = 0;
-			i = i + 1;
-		}
+		break;
 	}
-}
-void Left(vector<vector<int> > &board, int &i, int &j, int &bX, int &bY)
-{
-	int N = board[0].size();
-	int M = board.size();
-	if (j - 1 >= 0 && board[i][j - 1] != 4)
+	case 'u':
 	{
-		if (board[i][j - 1] == 3)
+		if (pX - 1 >= 0 && board[pX - 1][pY] != 4)
 		{
-			if (j - 2 >= 0)
+			if (pX - 1 == bX&&pY == bY)
 			{
-				board[i][j - 2] = 3;
-				board[i][j - 1] = 1;
-				board[i][j] = 0;
-				j--;
-				bY--;
+				if (bX - 1 >= 0 && board[bX - 1][bY] != 4)
+				{
+					bX--;
+					pX--;
+				}
+			}
+			else
+			{
+				pX--;
 			}
 		}
-		else
-		{
-			board[i][j - 1] = 1;
-			board[i][j] = 0;
-			j--;
-		}
+		break;
 	}
-}
-void Right(vector<vector<int> > &board, int &i, int &j, int &bX, int &bY)
-{
-	int N = board[0].size();
-	int M = board.size();
-	if (j + 1 < N && board[i][j + 1] != 4)
+	case 'd':
 	{
-		if (board[i][j + 1] == 3)
+		if (pX + 1 < M && board[pX + 1][pY] != 4)
 		{
-			if (j + 2 >= 0)
+			if (pX + 1 == bX&&pY == bY)
 			{
-				board[i][j + 2] = 3;
-				board[i][j + 1] = 1;
-				board[i][j] = 0;
-				j++;
-				bY++;
+				if (bX + 1 >= 0 && board[bX + 1][bY] != 4)
+				{
+					bX++;
+					pX++;
+				}
+			}
+			else
+			{
+				pX++;
 			}
 		}
-		else
-		{
-			board[i][j + 1] = 1;
-			board[i][j] = 0;
-			j++;
-		}
+		break;
+	}
+	default:
+		break;
 	}
 }
+
 
 int main()
 {
@@ -109,9 +96,7 @@ int main()
 	int N, M, S;
 	cin >> N >> M >> S;
 	vector<vector<int> > board(M, vector<int>(N, 0));
-	int X, Y;
-	int bX, bY;
-	int startX, startY;
+	int peopleX, peopleY;
 	int targetX, targetY;
 	int boxX, boxY;
 	for (size_t i = 0; i < M; i++)
@@ -128,8 +113,8 @@ int main()
 			}
 			if (board[i][j] == 1)
 			{
-				startX = i;
-				startY = j;
+				peopleX = i;
+				peopleY = j;
 			}
 			if (board[i][j] == 3)
 			{
@@ -141,68 +126,25 @@ int main()
 
 	for (size_t i = 0; i < S; i++)
 	{
-		X = startX;
-		Y = startY;
-		bX = boxX;
-		bY = boxY;
+		int pX = peopleX;
+		int pY = peopleY;
+		int bX = boxX;
+		int bY = boxY;
 		int nStep;
 		cin >> nStep;
-		bool flag = false;
+		bool bToDest = false;
 		for (size_t i = 0; i < nStep; i++)
 		{
-			if (flag)
-			{
-				break;
-			}
 			char chDirection;
 			cin >> chDirection;
-			switch (chDirection)
+			Move(board, chDirection, pX, pY, bX, bY);
+			if (bX==targetX&&bY==targetY)
 			{
-			case 'l':
-			{
-				Left(board, X, Y, bX, bY);
-				if (bX == targetX&&bY == targetY)
-				{
-					flag = true;
-				}
-				break;
-			}
-			case 'r':
-			{
-				Right(board, X, Y, bX, bY);
-				if (bX == targetX&&bY == targetY)
-				{
-					flag = true;
-				}
-				break;
-
-			}
-			case 'u':
-			{
-				Up(board, X, Y, bX, bY);
-				if (bX == targetX&&bY == targetY)
-				{
-					flag = true;
-				}
-				break;
-
-			}
-			case 'd':
-			{
-				Down(board, X, Y, bX, bY);
-				if (bX == targetX&&bY == targetY)
-				{
-					flag = true;
-				}
-				break;
-
-			}
-			default:
-				break;
+				bToDest = true;
 			}
 		}
 
-		if (flag)
+		if (bToDest)
 		{
 			cout << "YES" << endl;
 		}
@@ -210,18 +152,6 @@ int main()
 		{
 			cout << "NO" << endl;
 		}
-		for (size_t i = 0; i < M; i++)
-		{
-			for (size_t j = 0; j < N; j++)
-			{
-				if (board[i][j] != 4)
-				{
-					board[i][j] = 0;
-				}
-			}
-		}
-		board[startX][startY] = 1;
-		board[boxX][boxY] = 3;
 	}
 	return 0;
 }
