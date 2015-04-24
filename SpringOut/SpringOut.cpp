3a992f97b49ecdc7ab8d01a92baae0504aab8548
Tevic <tevic.tt@gmail.com>
2015-04-24 23:26:23 +0800

    update

--- SpringOut/SpringOut.cpp
+++ SpringOut/SpringOut.cpp
@@ -1,127 +1,205 @@
 #include "..\Head.h"
 
-
-int N, K;
-bool *flag = new bool[K + 1];
-
-bool vote(int **p, bool flag1, int index)
+bool HasPreferThanA(vector<vector<int> >& vPreferList,int iPeople,int A)
 {
-	bool result;
-	int counter;
-	if (flag1 == 1)
+	for (size_t i = 0; i < vPreferList[0].size(); i++)
 	{
-		counter = 0;
-		for (int i = 0; i < N; i++)
+		if (vPreferList[iPeople][i] == A)
 		{
-			for (int j = 0; j < K + 1; j++)
-			{
-				if (p[i][j] == index)
-				{
-					break;
-				}
-				if (p[i][j] == index + 1)
-				{
-					counter++;
-					break;
-				}
-			}
-		}
-		if (counter < N - counter)
-			return true;
-		else
 			return false;
+		}
+		if (vPreferList[iPeople][i]>A || vPreferList[iPeople][i]==0)
+		{
+			return true;
+		}
 	}
-	else
+}
+
+void Vote(vector<vector<int> >& vPreferList, vector<bool>& vSuccessList)
+{
+	int nPeople = vPreferList.size();
+	int nCandidate = vPreferList[0].size()-1;
+	for (size_t i = nCandidate; i >= 1; i--)
 	{
-		counter = 0;
-		int pos = 0;
-		for (pos = index + 1; pos < K + 1; pos++)
+		int nCountAgree = 0;
+		for (size_t j = 0; j < nPeople; j++)
 		{
-			if (flag[pos] == 1)
+			if (!HasPreferThanA(vPreferList, j, i))
 			{
-				break;
+				nCountAgree++;
 			}
 		}
-		if (pos == K + 1)
-			pos = 0;
-		for (int i = 0; i < N; i++)
+		if (nCountAgree>(nPeople/2))
 		{
-			for (int j = 0; j < K + 1; j++)
-			{
-				if (p[i][j] == index)
-				{
-					break;
-				}
-				if (p[i][j] == pos)
-				{
-					counter++;
-					break;
-				}
-			}
+			vSuccessList[nCandidate - 1] = true;
 		}
-		if (counter < N - counter)
-			return true;
 		else
-			return false;
+		{
+			vSuccessList[nCandidate - 1] = false;
+		}
 	}
-	return result;
 }
 
 int main()
 {
-#ifdef DEBUG_MODE
-	freopen("Input.txt", "r", stdin);
-#endif // DEBUG_MODE
-
-	cin >> N >> K;
-	int **p = new int *[N];
-	for (int i = 0; i < K + 1; i++)
-		flag[i] = 0;
-	for (int i = 0; i < N; i++)
-	{
-		p[i] = new int[K + 1];
-	}
-	for (int i = 0; i < N; i++)
-	{
-		for (int j = 0; j < K + 1; j++)
-			cin >> p[i][j];
-	}
-	int counter = 0;
-	for (int i = 0; i < N; i++)
-	{
-		for (int j = 0; j < K + 1; j++)
+	#ifdef DEBUG_MODE
+		freopen("Input.txt", "r", stdin);
+	#endif // DEBUG_MODE
+		int nPeople;
+		int nCandidate;
+		cin >> nPeople >> nCandidate;
+		vector<vector<int> > vPreferList(nPeople, vector<int>(nCandidate + 1));
+		vector<bool> vSuccessList(nCandidate + 1);
+		vSuccessList[nCandidate] = true;
+		for (size_t i = 0; i < nPeople; i++)
 		{
-			if (p[i][j] == 0)
+			for (size_t j = 0; j < nCandidate+1; j++)
 			{
-				counter++;
-				break;
+				cin>>vPreferList[i][j];
 			}
-			if (p[i][j] == K)
+		}
+		Vote(vPreferList, vSuccessList);
+		bool iSelected=0;
+		for (size_t i = 0; i < vSuccessList.size()-1; i++)
+		{
+			if (vSuccessList[i])
 			{
+				iSelected = i + 1;
 				break;
 			}
 		}
-	}
-	if (counter < N - counter)
-		flag[K] = 1;
-	else
-		flag[K] = 0;
-	int k = K - 1;
-	while (k >= 1)
-	{
-		flag[k] = vote(p, flag[k + 1], k);
-		k--;
-	}
-	int i;
-	for (i = 0; i < K + 1; i++)
-	{
-		if (flag[i] == 1)
+		if (iSelected!=0)
 		{
-			cout << i << endl;
-			break;
+			cout << iSelected << endl;
 		}
-	}
-	if (i == K + 1)
-		cout << "otaku" << endl;
-	return 0;
+		else
+		{
+			cout << "otaku" << endl;
+		}
+		return 0;
 }
+
+//int N, K;
+//bool *flag = new bool[K + 1];
+//
+//bool vote(int **p, bool flag1, int index)
+//{
+//	bool result;
+//	int counter;
+//	if (flag1 == 1)
+//	{
+//		counter = 0;
+//		for (int i = 0; i < N; i++)
+//		{
+//			for (int j = 0; j < K + 1; j++)
+//			{
+//				if (p[i][j] == index)
+//				{
+//					break;
+//				}
+//				if (p[i][j] == index + 1)
+//				{
+//					counter++;
+//					break;
+//				}
+//			}
+//		}
+//		if (counter < N - counter)
+//			return true;
+//		else
+//			return false;
+//	}
+//	else
+//	{
+//		counter = 0;
+//		int pos = 0;
+//		for (pos = index + 1; pos < K + 1; pos++)
+//		{
+//			if (flag[pos] == 1)
+//			{
+//				break;
+//			}
+//		}
+//		if (pos == K + 1)
+//			pos = 0;
+//		for (int i = 0; i < N; i++)
+//		{
+//			for (int j = 0; j < K + 1; j++)
+//			{
+//				if (p[i][j] == index)
+//				{
+//					break;
+//				}
+//				if (p[i][j] == pos)
+//				{
+//					counter++;
+//					break;
+//				}
+//			}
+//		}
+//		if (counter < N - counter)
+//			return true;
+//		else
+//			return false;
+//	}
+//	return result;
+//}
+//
+//int main()
+//{
+//#ifdef DEBUG_MODE
+//	freopen("Input.txt", "r", stdin);
+//#endif // DEBUG_MODE
+//
+//	cin >> N >> K;
+//	int **p = new int *[N];
+//	for (int i = 0; i < K + 1; i++)
+//		flag[i] = 0;
+//	for (int i = 0; i < N; i++)
+//	{
+//		p[i] = new int[K + 1];
+//	}
+//	for (int i = 0; i < N; i++)
+//	{
+//		for (int j = 0; j < K + 1; j++)
+//			cin >> p[i][j];
+//	}
+//	int counter = 0;
+//	for (int i = 0; i < N; i++)
+//	{
+//		for (int j = 0; j < K + 1; j++)
+//		{
+//			if (p[i][j] == 0)
+//			{
+//				counter++;
+//				break;
+//			}
+//			if (p[i][j] == K)
+//			{
+//				break;
+//			}
+//		}
+//	}
+//	if (counter < N - counter)
+//		flag[K] = 1;
+//	else
+//		flag[K] = 0;
+//	int k = K - 1;
+//	while (k >= 1)
+//	{
+//		flag[k] = vote(p, flag[k + 1], k);
+//		k--;
+//	}
+//	int i;
+//	for (i = 0; i < K + 1; i++)
+//	{
+//		if (flag[i] == 1)
+//		{
+//			cout << i << endl;
+//			break;
+//		}
+//	}
+//	if (i == K + 1)
+//		cout << "otaku" << endl;
+//	return 0;
+//}
